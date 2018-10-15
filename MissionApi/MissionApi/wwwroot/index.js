e5070a4d8ab754ecf6215b61871d189d48b956aa
Dosbol <dosbol_1997@mail.ru>
2018-10-16 02:41:45 +0600

    Add *

--- MissionApi/MissionApi/wwwroot/index.js
+++ MissionApi/MissionApi/wwwroot/index.js
@@ -2,14 +2,20 @@
 
 
 var vm1 = new Vue({
-    el: '#appp',
+    el: '#app',
     data: {
         endpoint: 'api/missions/get',
         posts: [],
         post: {
 
         },
-        search:''
+        search: '',
+        input: "ast",
+        id: "",
+        name: "",
+        tegs: "",
+        description: "",
+        complate: ""
 
 
     },
@@ -26,6 +32,28 @@ var vm1 = new Vue({
             }, function (error) {
 
             })
+
+        },
+        saveData: function () {
+
+            var name = this.name;
+            var tegs = this.tegs;
+            var description = this.description;
+            var complate = this.complate;
+            this.$http.post('api/missions/post', { name, tegs, description, complate }).then(function (response) {
+
+            }, function (response) {
+
+            })
+        },
+        reset: function () {
+            this.id = '';
+            this.name = '';
+            this.tegs = '';
+            this.description = '';
+            this.complate = '';
+
+
 
         },
 
@@ -59,42 +87,3 @@ var vm1 = new Vue({
     }
 })
 
-var vm2 = new Vue({
-    el: '#app',
-    data() {
-        return {
-            input: "ast",
-            id:"",
-            name: "",
-            tegs: "",
-            description: "",
-            complate: ""
-        }
-    }  
-    ,
-    methods: {
-        saveData: function () {
-            
-            var name = this.name;
-           var tegs = this.tegs;
-            var description = this.description;
-            var complate = this.complate;
-            this.$http.post('api/missions/post', {  name, tegs, description, complate }).then(function (response) {
-
-            }, function (response) {
-
-            })
-        },
-        reset: function () {
-            this.id = '';
-            this.name = '';
-            this.tegs = '';
-            this.description = '';
-            this.complate = '';
-
-
-
-        },
-
-    }
-})
